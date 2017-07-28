14e6dc47100ac46a1ecd12d80b7cd4d69df6013d
omer.kara <omer.kara@outlook.be>
2017-07-28 17:54:13 +0200

    first commit

---
+++ NoSkillTeam-PrivacyPassport/NoSkillTeam-PrivacyPassport/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoSkillTeam-PrivacyPassport.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
