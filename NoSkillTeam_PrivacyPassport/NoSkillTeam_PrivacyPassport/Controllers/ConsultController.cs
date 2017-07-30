bc87fa6dfa7432bfa2c612d4c16120b6f6c01f8f
Lehtonen Miika <miika@lehtonen.be>
2017-07-30 12:09:03 +0200

    add consult controller

---
+++ NoSkillTeam_PrivacyPassport/NoSkillTeam_PrivacyPassport/Controllers/ConsultController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoSkillTeam_PrivacyPassport.Controllers
+{
+    public class ConsultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
